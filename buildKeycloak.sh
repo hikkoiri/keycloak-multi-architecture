46bba166e72f83f5a94c4c1c012e5b95ac009224
Carlo Hildebrandt <carlo.hildebrandt@ibm.com>
2020-04-23 20:35:51 +0200

    bash supports ==

--- buildKeycloak.sh
+++ buildKeycloak.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$1" ]
 then
@@ -12,12 +12,12 @@ git clone https://github.com/keycloak/keycloak-containers
 cd keycloak-containers/server
 
 # here comes the interesting part
-# sed command needs to be different between mac & linux 
+# sed command needs to be different between mac & linux
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
     sed -i '' "s/registry\.access\.redhat\.com\/ubi8-minimal/fedora:29/g" Dockerfile
     sed -i '' "s/microdnf/dnf/g" Dockerfile
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
     sed -i "s/registry\.access\.redhat\.com\/ubi8-minimal/fedora:29/g" Dockerfile
     sed -i "s/microdnf/dnf/g" Dockerfile
